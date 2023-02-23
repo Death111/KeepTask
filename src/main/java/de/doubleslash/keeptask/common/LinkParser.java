0740c97856ed532c4e392d8482c361eb2189b9d6
Nico <5772511+Death111@users.noreply.github.com>
2023-02-23 10:05:23 +0100

    started MD links

--- src/main/java/de/doubleslash/keeptask/common/LinkParser.java
+++ src/main/java/de/doubleslash/keeptask/common/LinkParser.java
@@ -1,35 +1,71 @@
 package de.doubleslash.keeptask.common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class LinkParser {
 
-    // Regular expression to match URLs
     String urlRegex = "https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-    Pattern pattern = Pattern.compile(urlRegex);
+    String markdownRegex = "\\[([\\w\\s\\d]+)\\]\\((" + urlRegex + ")\\)";
+    Pattern linkOnlyPattern = Pattern.compile(urlRegex);
+    Pattern markdownPattern = Pattern.compile(markdownRegex);
 
     public List<TodoPart> splitAtLinks(String input) {
-        ArrayList<TodoPart> list = new ArrayList<>();
+        List<TodoPart> list = new ArrayList<>();
 
+        String[] split = input.split("\\s");
 
-        Matcher matcher = pattern.matcher(input);
+        String tmp = "";
+        for (String part : split) {
+            TodoPart mdLink = getMarkDownLink(part);
+            if (mdLink != null) {
+                if(!tmp.isEmpty()){
+                    list.add(new TodoPart(tmp));
+                    tmp = "";
+                }
+                list.add(mdLink);
+                continue;
+            }
 
-        // Find all URLs in the input string
-        int lastEnd = 0;
-        while (matcher.find()) {
-            // Append the normal text before the link
-            list.add(new TodoPart(input.substring(lastEnd, matcher.start())));
-            // link part
-            list.add(new TodoPart(matcher.group(), matcher.group()));
-            lastEnd = matcher.end();
+            TodoPart link = getLink(part);
+            if (link != null) {
+                if(!tmp.isEmpty()){
+                    list.add(new TodoPart(tmp));
+                    tmp = "";
+                }
+                list.add(link);
+                continue;
+            }
+
+            tmp += part + " ";
         }
 
-        if (lastEnd != input.length()) {
-            list.add(new TodoPart(input.substring(lastEnd)));
+        if(!tmp.isBlank()){
+            list.add(new TodoPart(tmp));
         }
+
         return list;
+    }
+
+    private TodoPart getMarkDownLink(String inputString) {
+        Matcher matcher = markdownPattern.matcher(inputString);
+        if (matcher.find()) {
+            String group1 = matcher.group(1);
+            String group2 = matcher.group(2);
+            return new TodoPart(group1, group2);
+        }
+        return null;
+    }
+
+    private TodoPart getLink(String inputString) {
+        Matcher matcher = linkOnlyPattern.matcher(inputString);
+        if (matcher.find()) {
+            return new TodoPart(matcher.group(), matcher.group());
+        }
+        return null;
     }
 }
